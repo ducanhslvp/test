a615861c03031a79220b4e97a2e719a2284ddb0e
ducanhslvp <ducanhslvp99@gmail.com>
2019-08-23 20:05:53 +0700

    add

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+class nhap{
+
+    public static void nhapsach(){
+        Scanner nhap=new Scanner(System.in);
+        System.out.println("Nhap ma sach: ");
+        int masach=nhap.nextInt();
+        nhap.nextLine();
+        System.out.println("Nhap ten sach: ");
+        String tensach=nhap.nextLine();
+        System.out.println("Nha xuat ban: ");
+        String xuatban=nhap.nextLine();
+        System.out.println("Tac gia : ");
+        String tacgia=nhap.nextLine();
+        System.out.println("Chuyen nganh: ");
+        String chuyennganh=nhap.nextLine();
+        System.out.println("So trang: ");
+        int sotrang=nhap.nextInt();
+        nhap.nextLine();
+    }
+}
+class
